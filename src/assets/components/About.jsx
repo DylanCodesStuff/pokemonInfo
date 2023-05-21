ca2d235c5bf0226005fee5d9830691ba6f68a483
DylanCodesStuff <dscriven16@gmail.com>
2023-05-20 23:43:43 -0400

    Added About page and styled.

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -1,5 +1,57 @@
 import React from "react";
-
+import "../styles/about.css";
 export default function Berries() {
-  return <h1>hi</h1>;
+  return (
+    <div className="page-container">
+      <div className="left-box">
+        <div className="img-container">
+          <img
+            alt="picture of creator"
+            className="dylan-image"
+            src="https://i.ibb.co/wRYM5Wp/IMG-3714.png"
+          />
+        </div>
+        <div className="div-one">
+          <h1>Who Am I?</h1>
+          <p>
+            I am a self-taught programmer who has built this website with no
+            guide or hand-holding. This is a complete bottoms-up project that
+            demonstrates my knowledge of full-stack development. Anyone can copy
+            a project they code along with from youtube, but I thought it would
+            be much more valuable to display my true, confident, and genuine
+            skills with this handcrafted project.
+          </p>
+        </div>
+      </div>
+      <div className="right-box">
+        <div className="div-two">
+          <h1>What's My Goal?</h1>
+          <p>
+            I am attempting to imporove my design and programming skills by
+            building projects that introduce me to new technologies and
+            reinforce familiar ones. This will set me up to become the best
+            developer I can be.
+          </p>
+        </div>
+
+        <div className="div-three">
+          <h1>What's Next</h1>
+          <p>
+            If you are a potential employer I would be very excited to hear from
+            you at one of the following contact points!
+          </p>
+          <ul>
+            <li>Email: dscriven16@gmail.com</li>
+            <li>Phone: 716-490-5909</li>
+            <li>
+              LinkedIn:{" "}
+              <a href="linkedin.com/in/dylanscriven">
+                linkedin.com/in/dylanscriven
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
